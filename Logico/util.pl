2f9df0a563991a5c6958197953e0db812b5bc2b2
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-01-11 15:05:39 -0300

    função de ler os arquivos

--- Logico/util.pl
+++ Logico/util.pl
@@ -1,1 +1,32 @@
 clearScr :- write('\33\[2J').
+
+
+lerArquivo(FilePath, Dados):-
+    open(FilePath,read,F),
+    ler_dados(F,Dados),
+    close(F).
+   
+ler_dados(F,[]):-
+    at_end_of_stream(F).
+   
+ler_dados(F,[X|L]):-
+    \+  at_end_of_stream(F),
+    read(F,X),
+    ler_dados(F,L).
+
+
+getDinheiro :-
+    lerArquivo('arquivos/dinheiro.txt', Dados),
+    write(Dados).
+
+
+getRepetidas :-
+    lerArquivo('arquivos/repetidas.txt', Dados),
+    write(Dados).
+
+
+getAlbum :-
+    lerArquivo('arquivos/album.txt', Dados),
+    write(Dados).
+
+
